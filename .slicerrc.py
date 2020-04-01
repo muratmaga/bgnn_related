c755f9e83234984a45bc1041fb5909af4fa77565
muratmaga <muratmaga@outlook.com>
2020-03-31 18:16:05 -0700

    Update .slicerrc.py

--- .slicerrc.py
+++ .slicerrc.py
@@ -1,6 +1,12 @@
 #make help as the default panel
 findChildren(name="HelpAcknowledgementTabWidget")[0].currentIndex = 0
 
+#hide SLicer logo in module tab
+slicer.util.findChild(slicer.util.mainWindow(), 'LogoLabel').visible = False
+
+#collapse Data Probe tab by default to save space modules tab
+slicer.util.findChild(slicer.util.mainWindow(), name='DataProbeCollapsibleWidget').collapsed = True
+
 #disable interpolation of the volumes by default
 def NoInterpolate(caller,event):
   for node in slicer.util.getNodes('*').values():
@@ -8,6 +14,3 @@ def NoInterpolate(caller,event):
       node.SetInterpolate(0)
 slicer.mrmlScene.AddObserver(slicer.mrmlScene.NodeAddedEvent, NoInterpolate)
 
-# Disable slice annotations persistently (after Slicer restarts)
-settings = qt.QSettings()
-settings.setValue('DataProbe/sliceViewAnnotations.enabled', 0)
